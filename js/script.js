de5b41a73f784b3ce37884075bd46a5ccc1ed1b5
Elsayed Hassan <eh600039@gmail.com>
2023-01-25 20:43:53 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
+
 let switcher = Array.from(document.querySelectorAll('.switcher li'));
 let imgs = Array.from(document.images); 
 
 switcher.forEach((li)=>{
     li.addEventListener("click", (e) => {
+            //display active class
             switcher.forEach((li) => {
                 li.classList.remove("active");
             });
+            //add active class
             e.currentTarget.classList.add("active");
+            //display all images
             imgs.forEach((img)=>{
                 img.style.display= "none";
             })
+            //show filterd images
             document.querySelectorAll(e.currentTarget.dataset.cat).forEach((el)=>{
                 el.style.display= "block";
             })
